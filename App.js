08fc04c9c95e3215e0348d01dfa6d4b35bdbccd0
Dhananjay Waghela <dhananjay@consumer-sketch.com>
2022-08-25 16:30:48 +0530

    Initial project setup

---
+++ App.js
@@ -0,0 +1,42 @@
+//Import react native hooks
+import React, {useEffect, useState} from 'react';
+
+//Import react native controls
+import {
+  StyleSheet,
+  Text,
+  Image,
+  FlatList,
+  View,
+  ActivityIndicator,
+  Alert,
+} from 'react-native';
+
+//Import react navigation
+import {NavigationContainer} from '@react-navigation/native';
+
+//Import react stack navigation
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+//Application main screen
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Cat Breeds"
+          component={CatBreedScreen}
+          options={{headerStyle: styles.headerStyle}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+//Create stack navigation
+const Stack = createNativeStackNavigator();
+
+//Cat breed list screen
+function CatBreedScreen() {
+
+}
